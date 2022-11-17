fa0dbeb6e3d9f82428f95faaa593ba4f0d7cc821
g.huelsz <g.huelsz@nki.nl>
2022-11-17 12:08:52 +0100

    exons

--- get_gene_exons.py
+++ get_gene_exons.py
@@ -23,14 +23,14 @@ def get_exon_regions(refseq: pd.DataFrame, gene: str) -> pd.DataFrame:
     '''Returns dataframe with start and end positions of all gene exons and
     whether they are cds or utr. Eg.:
     name2	chrom	name	        exon_id	tx_id	region_type	start	end
-        CD274	chr9	NM_001267706.1	1	    1	    UTR	        5450502	5450596
-        CD274	chr9	NM_001267706.1	2	    1	    UTR	        5456099	5456113
-        CD274	chr9	NM_001267706.1	2	    1	    CDS	        5456113	5456165
+    CD274	chr9	NM_001267706.1	1	    1	    UTR	        5450502	5450596
+    CD274	chr9	NM_001267706.1	2	    1	    UTR	        5456099	5456113
+    CD274	chr9	NM_001267706.1	2	    1	    CDS	        5456113	5456165
     '''
 
     gene_pos = refseq.query('name2 == @gene')
 
-    exon = gene_pos.copy(deep=True)
+    exon = gene_pos.copy(deep=True) 
     exon = exon.reset_index(drop=True)
     exon['tx_id'] = exon.index + 1
 
